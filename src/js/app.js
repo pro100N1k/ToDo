0b5f8954a2ee36431b261a2fe2e00531d30da7e8
pro100N1k <pro100N1ks@ukr.net>
2019-11-05 13:33:22 +0200

    Adding items in ToDo list

--- src/js/app.js
+++ src/js/app.js
@@ -21,17 +21,12 @@ class Application {
                     id: Date.now(),
                 };
 
-                todoItems.push(todoObject);
-                console.log(todoItems);
 
-                $('.board-content').append(
-                    ` <div class="board-item-wrapper">
-                         <div class="board-item">
-                            <div class="board-item__date">20.09.2019</div>
-                             <div class="board-item__title">
-                                ${todoObject.text}
-                             </div>
-                             <div class="board-item-action">
+                todoItems.push(
+                    ` <div class="content-item-wrapper">
+                         <div class="content-item">
+                            <div class="content-item__date">20.09.2019</div>
+                             <div class="content-item__action">
                                  <ul>
                                      <li class="action action--open">
                                          <i class="zmdi zmdi-eye"></i>
@@ -44,139 +39,39 @@ class Application {
                                      </li>
                                  </ul>
                              </div>
-                             <div class="board-item__text">
+                             <div class="content-item__title">
+                                ${todoObject.text}
+                             </div>
+                             <div class="content-item__text">
                                  <textarea name="1" cols="30" rows="10"></textarea> Тут буде відповідний текст (опис) задачі, який буде прихований і щоб його побачити потрібно
                                  атиснути на кнопку "Читати"
                              </div>
                          </div>
                      </div>`
                 );
+
+                console.log(todoItems);
+
             }
 
-            function show (array) {
-                $('.board-body').html(array);
+            function showTasks () {
+                $('.board-body').html(todoItems);
             }
 
-            function removeTodoItem (item) {
-                item.remove();
-            }
-
-            // $('.js-add-item').on('click', function() {
-            //     show(todoItems);
-            // });
-
-            $('.describe-block-btn').on('click' , function(e) {
+            $('.js-add-item').on('click', function(e) {
                 e.preventDefault();
-                const input = $('.describe-block input');
-                const text = $('.describe-block input').val().trim();
+                const input = $('.input-wrapper__input');
+                const text = $('.input-wrapper__input').val().trim();
 
-                if (text !== '') {
+                if (text.length > 0) {
                     addTodoItem(text);
                     input.val('');
-                }
-            });
-
-
-            $('.describe-block textarea').on('keyup', function() {
-                const _this = $(this);
-                const text = _this.val();
-            });
-
-
-            $('.describe-block-btn').on('click' , function() {
-                // const _this = $(this);
-                // const wrapper = _this.closest('.describe-block');
-                // const text = wrapper.find('.input-wrapper input');
-                // const textVal = text.val();
-
-                // message.text(text);
-
-                // if(textVal.length >= 1) {
-                //
-                //     text.removeClass('error');
-                //     $('.board-content').append(`
-                //   <div class="board-item-wrapper">
-                //         <div class="board-item">
-                //             <div class="board-item__date">20.09.2019</div>
-                //             <div class="board-item__title">
-                //                 Тут буде введений заголовк, який має дубвюатися, чи просто виводитися у два рядки
-                //             </div>
-                //             <div class="board-item-action">
-                //                 <ul>
-                //                     <li class="action action--open">
-                //                         <i class="zmdi zmdi-eye"></i>
-                //                     </li>
-                //                     <li class="action action--edit">
-                //                         <i class="zmdi zmdi-edit"></i>
-                //                     </li>
-                //                     <li class="action action--delete">
-                //                         <i class="zmdi zmdi-close"></i>
-                //                     </li>
-                //                 </ul>
-                //             </div>
-                //             <div class="board-item__text">
-                //                 <textarea name="1" cols="30" rows="10"></textarea> Тут буде відповідний текст (опис) задачі, який буде прихований і щоб його побачити потрібно
-                //                 атиснути на кнопку "Читати"
-                //             </div>
-                //         </div>
-                //     </div>
-                // `);
-                //
-                //
-                // } else {
-                //     text.addClass('error');
-                //     alert('Питаю щось!?');
-                // }
-
-
-            });
-
-            $('.board-item-action .action--open').on('click', function() {
-                const _this = $(this);
-                const wrapper = _this.closest('.board-item-wrapper');
-                const text = wrapper.find('.board-item__text');
-
-            });
-
-            $('.board-item-action .action--edit').on('click', function() {
-                const _this = $(this);
-                const wrapper = _this.closest('.board-item-wrapper');
-
-                console.log('edit');
-            });
-
-            $('.board-item-action .action--delete').on('click', function() {
-                const _this = $(this);
-                const wrapper = _this.closest($(document).find('.board-item-wrapper'));
-                // actionElem(_this);
-                removeTodoItem(wrapper);
-                console.log(1);
-                showMessage();
-            });
-
-            function showMessage () {
-                const message = confirm('Are you sure about that&');
-                if(message === true) {
-                    alert('OK')
-                } else {
-                    alert('To nne morochu sraku!');
+                    showTasks();
                 }
 
-            }
 
+            });
 
-            function actionElem (el) {
-
-                if (el.hasClass('action--delete')) {
-                    el.closest('.board-item-wrapper').remove();
-
-                } else {
-                    if(el.hasClass('action--edit')) {
-                        console.log('edit2');
-
-                    }
-                }
-            }
         });
 
     };
